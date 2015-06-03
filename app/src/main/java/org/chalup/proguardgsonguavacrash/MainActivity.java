c1c40b98053b0f001a924dc5a4374f906d5e98b0
Jerzy Chalupski <jerzy@getbase.com>
2015-06-03 10:23:14 +0200

    Setup test case

--- app/src/main/java/org/chalup/proguardgsonguavacrash/MainActivity.java
+++ app/src/main/java/org/chalup/proguardgsonguavacrash/MainActivity.java
@@ -1,5 +1,8 @@
 package org.chalup.proguardgsonguavacrash;
 
+import com.google.common.collect.LinkedHashMultimap;
+import com.google.gson.Gson;
+
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -8,5 +11,12 @@ public class MainActivity extends Activity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    LinkedHashMultimap<Long, String> map = LinkedHashMultimap.create();
+    map.put(1l, "1");
+    map.put(1l, "2");
+    map.put(1l, "3");
+
+    new Gson().toJson(map.asMap().get(1l));
   }
 }
